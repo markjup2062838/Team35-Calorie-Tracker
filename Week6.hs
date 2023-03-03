e0f59417166c387177c8a050b537b9f046481d1a
aarondv12 <105861158+aarondv12@users.noreply.github.com>
2023-03-03 16:49:12 +0000

    Add files via upload

---
+++ Week6.hs
@@ -0,0 +1,32 @@
+{- Week6.hs
+ This module illustrates the use of functions as values
+-}
+
+import Data.Char
+
+twice :: (Int -> Int) -> Int -> Int
+twice f x = f (f x)
+
+multiply :: Int -> Int -> Int
+multiply x y = x * y
+
+double :: Int -> Int
+double = multiply 2
+
+doubleAll :: [Int] -> [Int]
+doubleAll = map (*2)
+
+areDigits :: String -> [Bool]
+areDigits = map isDigit
+
+keepPositive :: [Int] -> [Int]
+keepPositive = filter (>0)
+
+keepDigits :: String -> String
+keepDigits = filter isDigit
+
+addUp :: [Int] -> Int
+addUp = foldr (+) 0 
+
+myConcat :: [[a]] -> [a]
+myConcat = foldr (++) []
